ecd9b07511a35f81097680cc7f62a980499ce4bf
nikolajsen121 <130678378+nikolajsen121@users.noreply.github.com>
2023-04-15 12:39:15 +0200

    nu kan den køre frem og bakke

--- main.ts
+++ main.ts
@@ -1,50 +1,12 @@
-radio.setFrequencyBand(0)
-radio.setTransmitPower(7)
+input.onButtonPressed(Button.A, function () {
+    radio.sendValue("button_a", 1)
+})
+input.onButtonPressed(Button.B, function () {
+    radio.sendValue("button_b", 0)
+})
+basic.showIcon(IconNames.SmallDiamond)
 radio.setGroup(1)
 basic.forever(function () {
-    if (input.buttonIsPressed(Button.B)) {
-        radio.sendString("r")
-        basic.showLeds(`
-            . . # . .
-            . . # # .
-            # # # # #
-            . . # # .
-            . . # . .
-            `)
-    } else if (input.buttonIsPressed(Button.A)) {
-        radio.sendString("l")
-        basic.showLeds(`
-            . . # . .
-            . # # . .
-            # # # # #
-            . # # . .
-            . . # . .
-            `)
-    } else if (input.rotation(Rotation.Pitch) > 25) {
-        radio.sendString("b")
-        basic.showLeds(`
-            . . # . .
-            . . # . .
-            # # # # #
-            . # # # .
-            . . # . .
-            `)
-    } else if (input.rotation(Rotation.Pitch) < -15) {
-        radio.sendString("f")
-        basic.showLeds(`
-            . . # . .
-            . # # # .
-            # # # # #
-            . . # . .
-            . . # . .
-            `)
-    } else {
-        basic.showLeds(`
-            . . . . .
-            . . . . .
-            . . . . .
-            . . . . .
-            . . . . .
-            `)
-    }
+    radio.sendValue("mgx", input.acceleration(Dimension.X))
+    radio.sendValue("mgy", input.acceleration(Dimension.X))
 })
