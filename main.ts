bd9b9bd6d41e707a7b5ceaa9df722ce21fb275fe
nikolajsen121 <130678378+nikolajsen121@users.noreply.github.com>
2023-04-15 14:13:26 +0200

    X akse drejer, Y akse hastighed, knapper er retning frem-tilbage

--- main.ts
+++ main.ts
@@ -8,5 +8,5 @@ basic.showIcon(IconNames.SmallDiamond)
 radio.setGroup(1)
 basic.forever(function () {
     radio.sendValue("mgx", input.acceleration(Dimension.X))
-    radio.sendValue("mgy", input.acceleration(Dimension.X))
+    radio.sendValue("mgy", input.acceleration(Dimension.Y))
 })
